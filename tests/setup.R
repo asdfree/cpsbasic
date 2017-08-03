32e99f552a5a9de92267a046da7cefa85b19e336
Anthony Damico <ajdamico@gmail.com>
2017-08-03 08:39:00 -0400

    cpsbasic draft

--- tests/setup.R
+++ tests/setup.R
@@ -6,8 +6,8 @@ cpsbasic_cat <-
 	get_catalog( "cpsbasic" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 50% of the records
-which_records <- sample( seq( nrow( cpsbasic_cat ) ) , round( nrow( cpsbasic_cat ) * 0.50 ) )
+# sample 25% of the records
+which_records <- sample( seq( nrow( cpsbasic_cat ) ) , round( nrow( cpsbasic_cat ) * 0.25 ) )
 
 # always sample year == 2017 & month == 3
 cpsbasic_cat <- unique( rbind( cpsbasic_cat[ which_records , ] , subset( cpsbasic_cat , year == 2017 & month == 3 ) ) )
